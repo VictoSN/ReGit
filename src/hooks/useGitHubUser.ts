bab5371128695470653f2c8fa7e7e3ff40969bd2
Victorio <victorionanda1105@gmail.com>
2026-08-12 03:08:29 +0800

    feat: added the search success

--- src/hooks/useGitHubUser.ts
+++ src/hooks/useGitHubUser.ts
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-import { GitHubRepo, GitHubUser } from '../api/github'
+import { getGitHubRepos, getGitHubUser } from '../api/github'
+import type {GitHubRepo, GitHubUser } from '../api/github'
+
+type Status = "idle" | "loading" | "notFound" | "error" | "success"
 
 function useGitHubUsers() {
-  const [user, setUser] = useState<GitHubUser | null>(null)
-  const [repos, setRepos] = useState<GitHubRepo[]>([])
+    const [user, setUser] = useState<GitHubUser | null>(null)
+    const [repos, setRepos] = useState<GitHubRepo[]>([])
+    const [status, setStatus] = useState<Status>("idle")
 
-  type Status = "idle" | "loading" | "notFound" | "error" | "success"
-  const [status, setStatus] = useState<Status>("idle")
+    const search = async(username: string) => {
+        setStatus("loading")
+        try {
+            const user = await getGitHubUser(username)
+            setUser(user)
 
+            const repos = await getGitHubRepos(username)
+            setRepos(repos)
 
+            setStatus("success")
+        } catch (error) {
 
+        }
+    }
+
+    return { user, repos, status, search }
 }
 
 export default useGitHubUsers
