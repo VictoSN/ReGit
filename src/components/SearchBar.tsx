5d7b16287b984d24bb00cb35df92958b4b1f9740
Victorio <victorionanda1105@gmail.com>
2026-08-12 05:48:43 +0800

    feat: added search icon

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,5 @@
-import logo from '../../public/logo-circle.png'
+import logoIcon from '../assets/logo-circle.png'
+import searchIcon from '../assets/search.svg'
 
 interface SearchBarProps {
     query: string
@@ -13,10 +14,13 @@ function SearchBar({ query, setQuery, onSearch }: SearchBarProps) {
         <div className="flex items-center w-full text-white px-5 border-solid border-b border-gray-600">
             <h1 className="text-left font-bold text-2xl">GH-Lookup</h1>
             <form className="flex mx-auto py-2 gap-5" onSubmit={(e) => {e.preventDefault(); onSearch() }}>
-                <input value={query} onChange={(e) => setQuery(e.target.value)} className="border border-orange-500 p-2 rounded-[30px] h-[40px] w-[560px] pl-6" placeholder="Find Anything"></input>
+                <div className='relative'>
+                    <img src={searchIcon} className="max-w-[20px] absolute left-3 top-1/2 -translate-y-1/2"/>
+                    <input value={query} onChange={(e) => setQuery(e.target.value)} className="border border-orange-500 rounded-[30px] h-[40px] w-[560px] pl-10" placeholder="Find Anything"></input>
+                </div>
             </form>
             <a href='https://github.com/VictoSN'>
-                <img src={logo} className="max-w-[40px]"/>
+                <img src={logoIcon} className="max-w-[40px]"/>
             </a>
         </div>
     )
