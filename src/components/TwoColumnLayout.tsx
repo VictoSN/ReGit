06d994a8e04babfdd58c0639e8db7c03b07a1d50
Victorio <victorionanda1105@gmail.com>
2026-08-22 02:51:45 +0800

    fix: added scrollbar for right column when its overflowing

--- src/components/TwoColumnLayout.tsx
+++ src/components/TwoColumnLayout.tsx
@@ -1,10 +1,11 @@
 import type { ReactNode } from "react";
+import "./TwoColumnLayout.css"
 
 function TwoColumnLayout({ left, right }: {left: ReactNode; right: ReactNode }) {
     return (
         <div className="w-[1020px] flex flex-row gap-5 text-white">
             <div className="flex flex-col w-7/10 gap-5">{left}</div>
-            <div className="flex flex-col w-3/10 sticky top-20 self-start gap-4">{right}</div>
+            <div className="scrollbar-hover flex flex-col w-3/10 sticky top-20 max-h-[calc(100vh-5rem)] overflow-y-auto self-start gap-4">{right}</div>
         </div>
     )
 }
